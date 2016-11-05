bc43470060d8c5ab3a1c0bd4073df32896e388ec
ngoldfar <ngoldfar@stevens.edu>
2016-11-05 10:57:22 -0400

    fixed constructor

--- val_perception/val_object_detection/find_button/include/find_button/FindButton.h
+++ val_perception/val_object_detection/find_button/include/find_button/FindButton.h
@@ -7,20 +7,20 @@
 #include <perception_common/MultisensePointCloud.h>
 #include <geometry_msgs/Point.h>
 #include <geometry_msgs/PointStamped.h>
- 
+#include <assert.h> 
 
 class FindButton
 {
 public:
 	//constructor takes in nodeHandle 
-	FindButton(src_perception::MultisenseImage() );  
+	FindButton(src_perception::MultisenseImage*);  
 	// safty function to see if there is a button
 	bool buttonDectected();
 	//get the location of the button
 	geometry_msgs::PointStamped getLocation();
 private:
 	//glabel node
-	src_perception::MultisenseImage() mi;
+	src_perception::MultisenseImage *mi;
 	//Proccess the image
 	geometry_msgs::Point processImage(cv::Mat);
 	//gather the nessary Sensor data
--- val_perception/val_object_detection/find_button/src/FindButton.cpp
+++ val_perception/val_object_detection/find_button/src/FindButton.cpp
@@ -2,9 +2,9 @@
 #include <find_button/FindButton.h>
 
 
-FindButton::FindButton(src_perception::MultisenseImage() *handle)
+FindButton::FindButton(src_perception::MultisenseImage *handle)
 {
-
-	
+	assert(handle != nullptr && "Multisesnse pointer is null");
+	mi = handle;
 }
 
