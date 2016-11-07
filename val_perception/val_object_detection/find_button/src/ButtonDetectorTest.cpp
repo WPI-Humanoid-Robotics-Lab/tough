fabf9052c55611e8a0032380201e3a0e30977bc1
ngoldfar <ngoldfar@stevens.edu>
2016-11-07 16:23:16 -0500

    error in test class

---
+++ val_perception/val_object_detection/find_button/src/ButtonDetectorTest.cpp
@@ -0,0 +1,20 @@
+
+
+#include <ros/ros.h>
+#include <find_button/ButtonDetector.h>
+#include <perception_common/MultisenseImage.h>
+
+
+int main(int argc, char** argv)
+ {
+	ros::init(argc,argv,"test_button");
+	ros::NodeHandle nh;
+	src_perception::MultisenseImage mi(nh);
+	ButtonDetector test(mi,nh);
+	// while(ros::ok())
+	// {
+	// 	ros::spinOnce();
+	// }
+
+
+}
