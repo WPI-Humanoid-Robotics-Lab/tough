b522f41a5107522fdc3ed9a7b6393e95f9e02408
Benjamin <bpinaya@wpi.edu>
2016-11-09 12:51:21 -0500

    finilized migration to current standar, issues with SEGFAULT, if you fetch this, be aware random stranger

--- led_detector/include/led_detector/LedDetector.h
+++ led_detector/include/led_detector/LedDetector.h
@@ -15,52 +15,131 @@
 // #include <opencv2/imgproc/imgproc.hpp>
 // #include <opencv2/highgui/highgui.hpp>
 
-#include <geometry_msgs/Point.h>
 #include <std_msgs/Int32MultiArray.h>
-// #include <sensor_msgs/PointCloud2.h>
 
 
-// Datatype for storing image frames
+/**
+ * @brief The ImageFrame class Datatype for storing image frames
+ */
 class ImageFrame
 {
-    public:
-        cv::Mat                     img;        //original image
-        cv::Mat                     disp;       //disparity image
-        cv::Mat                     Q;          //Q matrix
-        cv::Mat                     K;
+public:
+    /**
+     * @brief m_originalImage
+     */
+    cv::Mat                     m_originalImage;        //original image
 
-        ImageFrame() {}
+    /**
+     * @brief m_disparityImage
+     */
+    cv::Mat                     m_disparityImage;       //disparity image
 
-        /**
+    /**
+     * @brief m_qMatrix
+     */
+    cv::Mat                     m_qMatrix;          //Q matrix
+
+    /**
+     * @brief ImageFrame
+     */
+    ImageFrame() {}
+
+    /**
          * the copy constructor that ensures deep copy
          * @param i the image frame
          */
-        ImageFrame(ImageFrame& i)
-        {
-            img = i.img.clone();
-            disp = i.disp.clone();
-            Q = i.Q.clone();
-            K = i.K.clone();
-        }
+    ImageFrame(ImageFrame& i)
+    {
+        m_originalImage = i.m_originalImage.clone();
+        m_disparityImage = i.m_disparityImage.clone();
+        m_qMatrix = i.m_qMatrix.clone();
+        ROS_DEBUG("HEADER DETECTLED");
+    }
 };
 
-class LightDetector
+namespace src_qual1_task{
+/**
+ * @brief The LedDetector class
+ */
+class LedDetector
 {
 public:
-    src_perception::MultisenseImage         *multisenseImage;
-    src_perception::MultisensePointCloud    *multisensePc;
-    src_perception::LaserPointCloud::Ptr    laserCloud;//!!!
-    LightDetector(ros::NodeHandle);
-    ~LightDetector();
+
+    /**
+     * @brief LedDetector
+     * @param nh
+     */
+    LedDetector(ros::NodeHandle nh);
+
+    ~LedDetector();
+
+    /**
+     * @brief m_multisenseImagePtr
+     */
+    src_perception::MultisenseImage         *m_multisenseImagePtr;
+
+    /**
+     * @brief m_multisensePcPtr
+     */
+    src_perception::MultisensePointCloud    *m_multisensePcPtr;
+
 
     bool detectLed();
     void getLedColor();
     void getLedLocation();
-private:
-    ros::NodeHandle                         nh;
-    cv::Mat                                 stereoImage3D;
-    ros::Publisher                          pubLedCoordinatesCam;
-    // ros::Subscriber                         _scribble_sub;//!!!!!!!
-    ros::Publisher                          pubRGBColor;
 
+    /**
+     * @brief DetectLED
+     * @param new_image
+     *
+     * \todo : return the status as true or false
+     */
+    void DetectLED(const cv::Mat &new_image) ;
+private:
+    /**
+     * @brief m_stereoImage3D
+     */
+    cv::Mat                                 m_stereoImage3D;
+
+    /**
+     * @brief m_imageXYZpub
+     */
+    ros::Publisher                          m_imageXYZpub;
+
+    /**
+     * @brief m_imageRGBpub
+     */
+    ros::Publisher                          m_imageRGBpub;
+
+    /**
+     * @brief old_image
+     */
+    cv::Mat old_image;
+
+    /**
+     * @brief m_gradientContours
+     */
+    std::vector<std::vector<cv::Point> > m_gradientContours;
+    //    cv::RNG rng(12345);
+
+    /**
+     * @brief m_randomGen
+     */
+    cv::RNG m_randomGen;
+
+    /**
+     * @brief m_cvDepthPtr
+     */
+    cv_bridge::CvImagePtr m_cvDepthPtr;
+
+    /**
+     * @brief m_baseFrame
+     */
+    std::string m_baseFrame = "";
+
+    /**
+     * @brief m_fixedFrame
+     */
+    std::string m_fixedFrame = "";
 };
+}
