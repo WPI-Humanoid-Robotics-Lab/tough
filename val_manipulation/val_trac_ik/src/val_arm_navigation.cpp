6910c77e3cc510e5e972e50b897a71fd26462baa
sumanth <sumanth.724@gmail.com>
2016-11-21 20:34:25 -0500

    corrected

--- val_manipulation/val_trac_ik/src/val_arm_navigation.cpp
+++ val_manipulation/val_trac_ik/src/val_arm_navigation.cpp
@@ -1,7 +1,8 @@
 #include<val_manipulation/val_arm_navigation_.h>
 #include<stdlib.h>
+#include <stdio.h>
 
-ihmc_msgs::OneDoFJointTrajectoryRosMessage appendTrajectoryPoint(float time, float* pos);
+ihmc_msgs::ArmTrajectoryRosMessage appendTrajectoryPoint(ihmc_msgs::ArmTrajectoryRosMessage msg, float time, float* pos);
 
 int main(int argc, char** argv)
 {
@@ -10,8 +11,8 @@ int main(int argc, char** argv)
 
     ros::Rate loop_rate(10);
 
-    ros::Publisher armTrajectoryPublisher = nh.advertise<ihmc_msgs::ArmTrajectoryRosMessage>("arm_trajectory_publisher", 1,true);
-    ros::Publisher handTrajectoryPublisher = nh.advertise<ihmc_msgs::HandDesiredConfigurationRosMessage>("hand_desired_configuration", 1,true);
+    ros::Publisher armTrajectoryPublisher = nh.advertise<ihmc_msgs::ArmTrajectoryRosMessage>("/ihmc_ros/valkyrie/control/arm_trajectory", 1,true);
+    ros::Publisher handTrajectoryPublisher = nh.advertise<ihmc_msgs::HandDesiredConfigurationRosMessage>("/ihmc_ros/valkyrie/control/hand_desired_configuration", 1,true);
 
     ihmc_msgs::ArmTrajectoryRosMessage arm_traj;
 
@@ -19,33 +20,49 @@ int main(int argc, char** argv)
     float ELBOW_BENT_UP [] ={0.0, 0.0, 0.0, 2.0, 0.0, 0.0, 0.0};
     float BUTTON_PRESS [] ={0.0, 0.1, 0.2, 0.3, 0.0, 0.0, 0.0};
 
-    arm_traj.robot_side = ihmc_msgs::ArmTrajectoryRosMessage::RIGHT;
-    arm_traj.joint_trajectory_messages.push_back( appendTrajectoryPoint(2, ZERO_VECTOR));
-    arm_traj.joint_trajectory_messages.push_back( appendTrajectoryPoint(3, ELBOW_BENT_UP));
-    arm_traj.joint_trajectory_messages.push_back( appendTrajectoryPoint(4, ZERO_VECTOR));
-    arm_traj.joint_trajectory_messages.push_back( appendTrajectoryPoint(5, BUTTON_PRESS));
 
-    armTrajectoryPublisher.publish(arm_traj);
+    arm_traj.joint_trajectory_messages.resize(7);
+    arm_traj.robot_side = arm_traj.RIGHT;
+    arm_traj.unique_id = -1;
+    arm_traj = appendTrajectoryPoint(arm_traj,2, ZERO_VECTOR);
+    arm_traj = appendTrajectoryPoint(arm_traj, 3, ELBOW_BENT_UP);
+    arm_traj = appendTrajectoryPoint (arm_traj, 4, ZERO_VECTOR);
+    arm_traj = appendTrajectoryPoint(arm_traj, 5, BUTTON_PRESS);
+
+    //armTrajectoryPublisher.publish(arm_traj);
+
+    ihmc_msgs::HandDesiredConfigurationRosMessage hand_msg;
+    hand_msg.robot_side = hand_msg.RIGHT;
+    hand_msg.hand_desired_configuration = hand_msg.PINCH_GRIP;
+    hand_msg.unique_id = -2;
+    handTrajectoryPublisher.publish(hand_msg);
+
+    ros::Duration(1).sleep();
+
+    //  while (ros::ok())
+    // {
 
     ros::spinOnce();
     loop_rate.sleep();
+    //}
 
     return 0;
 }
 
-ihmc_msgs::OneDoFJointTrajectoryRosMessage appendTrajectoryPoint(float time, float* pos)
+ihmc_msgs::ArmTrajectoryRosMessage appendTrajectoryPoint(ihmc_msgs::ArmTrajectoryRosMessage msg, float time, float* pos)
 {
 
     ihmc_msgs::TrajectoryPoint1DRosMessage p;
     ihmc_msgs::OneDoFJointTrajectoryRosMessage t;
 
-    for (int i=1;i<7;i++)
+    for (int i=0;i<7;i++)
     {
         p.time = time;
         p.position = pos[i];
         p.velocity = 0;
         t.trajectory_points.push_back(p);
+        msg.joint_trajectory_messages[i].trajectory_points.push_back(p);
     }
 
-    return t;
+    return msg;
 }
