467c183da46b7da1d63b8b8c504499e46e7fccd4
Vinayak Jagtap <vinayak.jagtap04@gmail.com>
2016-12-13 06:49:50 +0000

    Removed prompting while installing ROS

--- val_common/scripts/setup1.sh
+++ val_common/scripts/setup1.sh
@@ -17,7 +17,7 @@ else
   sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
   sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA116
   sudo apt-get update
-  sudo apt-get install ros-indigo-desktop-full
+  sudo apt-get install -y ros-indigo-desktop-full
   sudo rosdep init
   rosdep update
   echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
