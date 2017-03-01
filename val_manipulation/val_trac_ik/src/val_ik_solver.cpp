4114cdde433e5c11ede1b815a4221c1d61b2cbd1
Vinayak Jagtap <vinayak.jagtap04@gmail.com>
2017-03-01 17:49:51 -0500

    Modified moveit config to use tracIK

--- val_manipulation/val_trac_ik/src/val_ik_solver.cpp
+++ val_manipulation/val_trac_ik/src/val_ik_solver.cpp
@@ -15,32 +15,32 @@ int main(int argc, char** argv)
     std::string chain_start, chain_end, urdf_param;
     double timeout;
 
-    nh.param("num_samples", num_samples, 1);
-    nh.param("chain_start", chain_start, std::string(""));
-    nh.param("chain_end", chain_end, std::string(""));
+    nh.param("num_samples", num_samples, 20);
+    nh.param("chain_start", chain_start, std::string("torso"));
+    nh.param("chain_end", chain_end, std::string("rightPalm"));
 
     if (chain_start=="" || chain_end=="") {
         ROS_FATAL("Missing chain info in launch file");
         exit (-1);
     }
 
-    nh.param("timeout", timeout, 0.005);
+    nh.param("timeout", timeout, 5.0);
     nh.param("urdf_param", urdf_param, std::string("/robot_description"));
 
     if (num_samples < 1)
         num_samples = 1;
-    while(ros::ok() && loop==1) {
-        try {
-            //ROS_INFO("trying");
-            listener.waitForTransform("/buttonFrame", "/torso",ros::Time(0), ros::Duration(10.0));
-            listener.lookupTransform("/buttonFrame", "/torso", ros::Time(0), transform);
-            loop=0;
-        }
-        catch (tf::TransformException ex) {
-           ROS_ERROR("%s",ex.what());
-           ros::Duration(0.3).sleep();
-        }
-    }
+//    while(ros::ok() && loop==1) {
+//        try {
+//            //ROS_INFO("trying");
+//            listener.waitForTransform("/buttonFrame", "/torso",ros::Time(0), ros::Duration(10.0));
+//            listener.lookupTransform("/buttonFrame", "/torso", ros::Time(0), transform);
+//            loop=0;
+//        }
+//        catch (tf::TransformException ex) {
+//           ROS_ERROR("%s",ex.what());
+//           ros::Duration(0.3).sleep();
+//        }
+//    }
 
 
 //    Translation: [0.836, -1.850, -2.895]
@@ -48,18 +48,18 @@ int main(int argc, char** argv)
 //              in RPY (radian) [3.142, -1.440, 1.571]
 //              in RPY (degree) [179.998, -82.500, 90.001]
 
-//    tf::Point point;
-//    point.setX(/*-0.177731); //(*/0.545);
-//    point.setY(/*-0.605271); //(*/-1.395);
-//    point.setZ(0.0756957); //(-2.895);
-//    tf::Quaternion q;
-//    q.setX(0.532);
-//    q.setY(0.532);
-//    q.setZ(0.466);
-//    q.setW(-0.466);
+    tf::Point point;
+    point.setX(0.8);
+    point.setY(0.4);
+    point.setZ(1.7); //(-2.895);
+    tf::Quaternion q;
+    q.setX(0.0);
+    q.setY(0.0);
+    q.setZ(0.0);
+    q.setW(1.0);
 
-//    transform.setOrigin(point);
-//    transform.setRotation(q);
+    transform.setOrigin(point);
+    transform.setRotation(q);
 
     ik_solver.solve_ik(num_samples, chain_start, chain_end, timeout, urdf_param, transform, result);
 
