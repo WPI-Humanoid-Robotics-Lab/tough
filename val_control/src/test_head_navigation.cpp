418bb1ed4ea9d049a6491eb0b645a04fa6bf480f
Kyle Corry <kncorry@wpi.edu>
2017-03-04 18:10:48 -0500

    Add quaternion example

--- val_control/src/test_head_navigation.cpp
+++ val_control/src/test_head_navigation.cpp
@@ -9,8 +9,15 @@ int main(int argc, char **argv)
 
   HeadTrajectory headTraj(nh);
 
-  if(argc != 4)
-    headTraj.moveHead(0, 0, 20, 2.0);
+  if(argc != 4){
+    geometry_msgs::Quaternion q;
+    q.w = 1.0;
+    q.x = 0.0;
+    q.y = 0.0;
+    q.z = 1.0;
+      headTraj.moveHead(q);
+    }
+    // headTraj.moveHead(0, 0, 20, 2.0);
   else {
     float roll = std::atof(argv[1]);
     float pitch = std::atof(argv[2]);
