e135b40477d2bcc05dec59c2b54fd44e5240f45d
sumanth <sumanth.724@gmail.com>
2017-03-04 19:56:51 -0500

    modified the arm navigation to integrate the moveit test pipeline

--- val_motion_planners/val_moveit/val_moveit_tasks/src/test_moveit.cpp
+++ val_motion_planners/val_moveit/val_moveit_tasks/src/test_moveit.cpp
@@ -38,16 +38,13 @@ int main(int argc, char** argv){
         std::cout << *it << ": " << *itd << std::endl;
     }
 
-    //    ros::Publisher display_publisher = node_handle.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
-    //    moveit_msgs::DisplayTrajectory display_trajectory;
-
     ROS_INFO("Reference frame: %s", group.getPlanningFrame().c_str());
     ROS_INFO("Reference frame: %s", group.getEndEffectorLink().c_str());
 
     // set the target location
     geometry_msgs::Pose target_pose;
     target_pose.orientation.w = 1.0;
-    target_pose.position.x = 0.28;
+    target_pose.position.x = 0.38;
     target_pose.position.y = -0.7;
     target_pose.position.z = 1.0;
     group.setPoseTarget(target_pose);
@@ -55,7 +52,7 @@ int main(int argc, char** argv){
 
     moveit::planning_interface::MoveGroup::Plan my_plan;
     bool success = group.plan((my_plan));
-
+    my_plan.trajectory_.joint_trajectory;
     if (success)
     {
         std::cout<<"sucessfully planned the trajectory"<<std::endl;
@@ -71,31 +68,9 @@ int main(int argc, char** argv){
         /* Sleep to give Rviz time to visualize the plan. */
         sleep(2.0);
 
-        armTrajectory armTraj(node_handle);
-
-        // fill the trajectory points
-        //    ihmc_msgs::ArmTrajectoryRosMessage p;
-        //    p.joint_trajectory_messages
-
-        std::vector<armTrajectory::armJointData> trajPoints;
-        trajPoints.clear();
-        for (std::size_t i = 0; i < my_plan.trajectory_.joint_trajectory.points.size(); ++i)
-        {
-            trajectory_msgs::JointTrajectoryPoint pt = my_plan.trajectory_.joint_trajectory.points[i];
-            armTrajectory::armJointData r;
-            r.side = RIGHT;
-            for (std::size_t j=0; j < pt.positions.size(); j++)
-            {
-                r.arm_pose.push_back(pt.positions[j]);
-            }
-            r.time = 0.1;
-            trajPoints.push_back(r);
-        }
-
-        // execute the plan on robot
         ROS_INFO("executing on robot\n");
-        armTraj.moveArmJoints(trajPoints);
-
+        armTrajectory armTraj(node_handle);
+        armTraj.moveArmTrajectory(RIGHT, my_plan.trajectory_.joint_trajectory);
     }
     else
     {
