6d92326d01963a14424257f1aea7f1f37392da0d
sumanth <sumanth.724@gmail.com>
2017-03-08 21:14:28 -0500

    moved the state execution code to a separate cpp file

--- val_tasks/val_task1/src/val_task1.cpp
+++ val_tasks/val_task1/src/val_task1.cpp
@@ -16,4 +16,97 @@ using namespace std;
 
 #define foreach BOOST_FOREACH
 
+bool preemptiveWait(double ms, decision_making::EventQueue& queue) {
+  ROS_INFO("prempting");
+  for (int i = 0; i < 100 && !queue.isTerminated(); i++)
+    boost::this_thread::sleep(boost::posix_time::milliseconds(ms / 100.0));
 
+  return queue.isTerminated();
+}
+
+// state machine state executions
+decision_making::TaskResult initTask(string name, const FSMCallContext& context, EventQueue& eventQueue)
+{
+  ROS_INFO_STREAM("executing " << name);
+
+  // the state transition can happen from an event externally or can be geenerated here
+  //!!!!! depends on the developer and use case
+
+  // generate the event
+  //eventQueue.riseEvent("/INIT_SUCESSUFL");
+
+  // wait infinetly until an external even occurs
+  while(!preemptiveWait(1000, eventQueue)){
+     ROS_INFO("waiting for transition");
+  }
+
+  return TaskResult::SUCCESS();
+}
+
+
+decision_making::TaskResult walkToControlPanelTask(string name, const FSMCallContext& context, EventQueue& eventQueue)
+{
+  ROS_INFO_STREAM("executing " << name);
+
+  eventQueue.riseEvent("/REACHED_PANEL");
+  return TaskResult::SUCCESS();
+}
+
+decision_making::TaskResult controlPitchTask(string name, const FSMCallContext& context, EventQueue& eventQueue)
+{
+  ROS_INFO_STREAM("executing " << name);
+
+  eventQueue.riseEvent("/PITCH_CORRECTION_SUCESSFUL");
+  return TaskResult::SUCCESS();
+}
+
+decision_making::TaskResult controlYawTask(string name, const FSMCallContext& context, EventQueue& eventQueue)
+{
+  ROS_INFO_STREAM("executing " << name);
+
+  eventQueue.riseEvent("/YAW_CORRECTION_SUCESSFUL");
+  return TaskResult::SUCCESS();
+}
+
+decision_making::TaskResult walkToEndTask(string name, const FSMCallContext& context, EventQueue& eventQueue)
+{
+  ROS_INFO_STREAM("executing " << name);
+
+  eventQueue.riseEvent("/WALK_TO_END");
+  return TaskResult::SUCCESS();
+}
+
+decision_making::TaskResult endTask(string name, const FSMCallContext& context, EventQueue& eventQueue)
+{
+  ROS_INFO_STREAM("executing " << name);
+
+  eventQueue.riseEvent("/STOP_TIMEOUT");
+  return TaskResult::SUCCESS();
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "task1");
+  ros_decision_making_init(argc, argv);
+  ros::NodeHandle nh;
+  RosEventQueue* q = new RosEventQueue();
+
+  ROS_INFO("Preparing Task1...");
+
+  // register the api's for states
+  LocalTasks::registrate("STATE_INIT", initTask);
+  LocalTasks::registrate("STATE_WALK_TO_CONTROL", walkToControlPanelTask);
+  LocalTasks::registrate("STATE_CORRECT_PITCH", controlPitchTask);
+  LocalTasks::registrate("STATE_CORRECT_YAW", controlYawTask);
+  LocalTasks::registrate("STATE_WALK_TO_FINISH", walkToEndTask);
+  LocalTasks::registrate("END_STATE", endTask);
+
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+  ROS_INFO("Starting Task1");
+  Fsmval_task1(NULL, q, "val_task1");
+
+  spinner.stop();
+
+  return 0;
+}
