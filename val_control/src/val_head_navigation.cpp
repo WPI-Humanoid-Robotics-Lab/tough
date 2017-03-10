6b27594b2a17617e651f4589d54e460f8a97a2f2
Kyle Corry <kncorry@wpi.edu>
2017-03-10 13:49:04 -0500

    Call overloaded moveHead function to avoid duplication of code

--- val_control/src/val_head_navigation.cpp
+++ val_control/src/val_head_navigation.cpp
@@ -35,33 +35,15 @@ void HeadTrajectory::appendNeckTrajectoryPoint(ihmc_msgs::NeckTrajectoryRosMessa
 
 void HeadTrajectory::moveHead(float roll, float pitch, float yaw, const float time)
 {
-  ihmc_msgs::HeadTrajectoryRosMessage msg;
-  ihmc_msgs::SO3TrajectoryPointRosMessage data;
-
   roll = degToRad * roll;
   pitch = degToRad * pitch;
   yaw = degToRad * yaw;
 
-  data.time = time;
   tf::Quaternion q;
   q.setRPY(roll, pitch, yaw);
-  tf::quaternionTFToMsg(q, data.orientation);
-  geometry_msgs::Vector3 v;
-  v.x = 0.0;
-  v.y = 0.0;
-  v.z = 0.0;
-  data.angular_velocity = v;
-
-  HeadTrajectory::head_id--;
-  msg.unique_id = HeadTrajectory::head_id;
-  msg.execution_mode = msg.OVERRIDE;
-
-  msg.taskspace_trajectory_points.clear();
-
-  msg.taskspace_trajectory_points.push_back(data);
-
-  // publish the message
-  headTrajPublisher.publish(msg);
+  geometry_msgs::Quaternion quaternion;
+  tf::quaternionTFToMsg(q, quaternion);
+  moveHead(quaternion, time);
 }
 
 void HeadTrajectory::moveHead(const geometry_msgs::Quaternion &quaternion, const float time)
