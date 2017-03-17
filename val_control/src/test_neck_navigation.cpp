9a812f008280cd2b7cc82aeddd4ae10bbd2bc9ad
Kyle Corry <kncorry@wpi.edu>
2017-03-17 19:20:16 +0000

    Neck and Head control

---
+++ val_control/src/test_neck_navigation.cpp
@@ -0,0 +1,17 @@
+#include <val_control/val_head_navigation.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "test_neck_navigation");
+  ros::NodeHandle nh;
+  ROS_INFO("Moving the neck");
+
+  HeadTrajectory headTraj(nh);
+
+  headTraj.moveNeckJoints({{ 0.0f, -1.0f, 0.0f }}, 2.0f);
+
+  ros::spin();
+
+  return 0;
+}
