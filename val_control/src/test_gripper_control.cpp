f022680a2a01c18906e028a484c2d0b9fdea8117
Sahil kejriwal <skejriwal@wpi.edu>
2017-03-18 00:01:06 +0000

    Controller for the grippers

---
+++ val_control/src/test_gripper_control.cpp
@@ -0,0 +1,34 @@
+#include <val_control/val_gripper_control.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv){
+
+    ros::init(argc, argv, "test_gripper_control");
+    ros::NodeHandle nh;
+    gripperControl gripcont(nh);
+
+    if(argc == 2 ){
+        if (argv[1][0] == '1'){
+            ROS_INFO("opening grippers");
+            gripcont.openGripper(LEFT);
+            gripcont.openGripper(RIGHT);
+        }
+        else {
+            ROS_INFO("closing grippers");
+            gripcont.closeGripper(LEFT);
+            gripcont.closeGripper(RIGHT);
+        }
+
+    }
+    else{
+        ROS_INFO("Usage: rosrun %s 1 \n to open grippers \n running demo", argv[0]);
+        std::vector<double> leftgripClose = {1.4, -0.55, 0.0, -0.9, -1.0};
+        std::vector<double> rightgripClose = {1.4, 0.55, 0.0, 0.9, 1.0};
+
+        gripcont.controlGripper(LEFT,leftgripClose);
+        gripcont.controlGripper(RIGHT,rightgripClose);
+    }
+    ros::spin();
+
+    return 0;
+}
