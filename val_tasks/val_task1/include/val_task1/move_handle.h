39d6087db381021089ff97256946ec78d615d321
Your Name <you@example.com>
2017-05-05 17:41:33 -0400

    put markery array around the hand parrelle to the plane

--- val_tasks/val_task1/include/val_task1/move_handle.h
+++ val_tasks/val_task1/include/val_task1/move_handle.h
@@ -7,17 +7,18 @@
 #include <visualization_msgs/MarkerArray.h>
 #include <visualization_msgs/Marker.h>
 #include <val_common/val_common_names.h>
-
+#include <val_control/robot_state.h>
 
 
 class move_handle{
 
   ros::Publisher array_pub;
   ros::NodeHandle nh_;
+  RobotStateInformer* robot_state_;
 
 public:
   move_handle (ros::NodeHandle);
-  void createCircle(geometry_msgs::PoseStamped center, float,float,float,float );
+  void createCircle(geometry_msgs::PoseStamped center, int, float,float,float,float );
   void visulatize(std::vector<geometry_msgs::PoseStamped>&);
 
 };
