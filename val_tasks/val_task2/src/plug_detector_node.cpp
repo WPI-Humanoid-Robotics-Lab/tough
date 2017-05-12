38c4e2fd95ab7d68a3cdeb9c52018f98efbaffcc
Your Name <you@example.com>
2017-05-12 19:13:24 -0400

    Working on Staircase detection

--- val_tasks/val_task2/src/plug_detector_node.cpp
+++ val_tasks/val_task2/src/plug_detector_node.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char** argv)
 {
-    ros::init (argc,argv,"findCableDetector");
+    ros::init (argc,argv,"findPlugDetector");
     ros::NodeHandle nh;
     int numIterations = 0;
     bool foundPlug = false;
@@ -12,7 +12,7 @@ int main(int argc, char** argv)
     while (!foundPlug && numIterations < 20)
     {
         foundPlug = p1.findPlug(PlugLoc);
-        //ROS_INFO(foundPlug ? "***** Cable detected" : "xxxxx Cable not detected");
+        //ROS_INFO(foundPlug ? "***** Plug detected" : "xxxxx Plug not detected");
         numIterations++;
     }
 
