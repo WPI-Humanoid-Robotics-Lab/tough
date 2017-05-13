10715bd38f41a41a523328c8adeb47f78355fbfe
sumanth <sumanth.724@gmail.com>
2017-05-12 20:47:30 -0400

    task1utils.h

--- val_tasks/val_task1/include/val_task1/val_task1_utils.h
+++ val_tasks/val_task1/include/val_task1/val_task1_utils.h
@@ -1,7 +1,12 @@
 #pragma once
 
 #include <ros/ros.h>
-#include<srcsim/Satellite.h>
+#include <srcsim/Satellite.h>
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/Pose.h>
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <val_common/val_common_names.h>
 
 class task1Utils {
 private:
@@ -9,7 +14,12 @@ private:
     ros::Subscriber satellite_sub_;
     srcsim::Satellite msg_;
 
+    ros::Publisher marker_pub_;
     void satelliteMsgCB (const srcsim::Satellite &msg);
+
+public:
+    task1Utils(ros::NodeHandle nh);
+    ~task1Utils();
 
     bool isPitchCorrectNow(void);
     bool isYawCorrectNow(void);
@@ -17,8 +27,6 @@ private:
     bool isYawCompleted(void);
     double getPitchDiff (void);
     double getYawDiff (void);
-
-public:
-    task1Utils(ros::NodeHandle nh);
-    ~task1Utils();
+    void getCircle3D (geometry_msgs::Point center, geometry_msgs::Point start,geometry_msgs::Pose pose, const std::vector<float> planeCoeffs, std::vector<geometry_msgs::Pose> &points, float radius, int steps =10);
+    void visulatise6DPoints(std::vector<geometry_msgs::Pose> &points);
 };
