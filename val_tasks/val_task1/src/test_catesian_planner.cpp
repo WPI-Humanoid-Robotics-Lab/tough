0af8cec5fafbd1227ca02e7885377a93ee83d422
shlok <ssagarwal@wpi.edu>
2017-05-14 17:16:22 -0400

    minor changes to test cartesian planner

--- val_tasks/val_task1/src/test_catesian_planner.cpp
+++ val_tasks/val_task1/src/test_catesian_planner.cpp
@@ -19,24 +19,29 @@ int main(int argc, char** argv){
     ros::Publisher display_publisher = node_handle.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
     moveit_msgs::DisplayTrajectory display_trajectory;
 
-    geometry_msgs::Point center;
-    center.x = 2.3;
-    center.y = -1.16;
-    center.z = 0.846;
+    geometry_msgs::Point centerWorld,centerPelvis;
 
-    geometry_msgs::Point start;
-    start.x = 2.32;
-    start.y = -0.95;
-    start.z = 0.849;
+    centerPelvis.x = 0.576;
+    centerPelvis.y= -0.196;
+    centerPelvis.z = -0.139;
+    robot_state->transformPoint(centerPelvis,centerWorld,VAL_COMMON_NAMES::PELVIS_TF);
+
+
+    geometry_msgs::Point startWorld,startPelvis;
+
+    startPelvis.x = 0.432;
+    startPelvis.y= -0.189;
+    startPelvis.z = -0.138;
+    robot_state->transformPoint(startPelvis,startWorld,VAL_COMMON_NAMES::PELVIS_TF);
 
     std::vector<geometry_msgs::Pose> points;
-    std::vector<float> panelCoeffs {-0.0261, 0.4502, 0.8925, -0.1001};
+    std::vector<float> panelCoeffs {-0.002, 0.4591, 0.8884, -0.1502};
 
     //desired pose
     geometry_msgs::Pose grasp_pose;
     robot_state->getCurrentPose("/rightMiddleFingerPitch1Link",grasp_pose);
 
-    task1_utils.getCircle3D(center, start, grasp_pose, panelCoeffs, points, 0.125, 10);
+    task1_utils.getCircle3D(centerWorld, startWorld, grasp_pose, panelCoeffs, points, 0.125, 10);
     task1_utils.visulatise6DPoints(points);
     ROS_INFO("waypoints generated");
 
@@ -47,21 +52,21 @@ int main(int argc, char** argv){
 
 
     moveit::planning_interface::MoveGroup::Plan my_plan;
-    if (1)
-    {
-      ROS_INFO("Visualizing plan 1 (again)");
-      display_trajectory.trajectory_start = my_plan.start_state_;
-      display_trajectory.trajectory.push_back(trajectory);
-      display_publisher.publish(display_trajectory);
-      /* Sleep to give Rviz time to visualize the plan. */
-      sleep(5.0);
-      ros::spin();
-  }
+    //    if (1)
+    //    {
+    //        ROS_INFO("Visualizing plan 1 (again)");
+    //        display_trajectory.trajectory_start = my_plan.start_state_;
+    //        display_trajectory.trajectory.push_back(trajectory);
+    //        display_publisher.publish(display_trajectory);
+    //        /* Sleep to give Rviz time to visualize the plan. */
+    //        sleep(5.0);
+    //        ros::spin();
+    //    }
 
 
 
 
-//    ROS_INFO("executing on robot\n");
-//    armTrajectory armTraj(node_handle);
-//    armTraj.moveArmTrajectory(RIGHT, trajectory.joint_trajectory);
+    //    ROS_INFO("executing on robot\n");
+    //    armTrajectory armTraj(node_handle);
+    //    armTraj.moveArmTrajectory(RIGHT, trajectory.joint_trajectory);
 }
