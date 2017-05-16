5c36abf0c967ef903f2aacd241a078b4fd1decfd
shlok <ssagarwal@wpi.edu>
2017-05-16 15:19:15 -0400

    grasping the solar pannel completed

--- val_tasks/val_task2/include/val_task2/solar_panel_grasp.h
+++ val_tasks/val_task2/include/val_task2/solar_panel_grasp.h
@@ -7,6 +7,8 @@
 #include <val_control/val_arm_navigation.h>
 #include <val_control/val_gripper_control.h>
 #include <val_control/robot_state.h>
+#include "val_moveit_planners/val_cartesian_planner.h"
+#include "val_control/val_wholebody_manipulation.h"
 
 #define Y_OFFSET 0.05
 
@@ -28,14 +30,17 @@ private:
    armTrajectory armTraj_;
    gripperControl gripper_;
    RobotStateInformer *current_state_;
+   cartesianPlanner* right_arm_planner_;
+   cartesianPlanner* left_arm_planner_;
+   wholebodyManipulation* wholebody_controller_;
    geometry_msgs::QuaternionStamped leftHandOrientation_ ;
    geometry_msgs::QuaternionStamped rightHandOrientation_;
 
    /*Top Grip*/
-   const std::vector<float> leftShoulderSeed_ = {-0.23, -0.72, 0.65, -1.51, 2.77, 0.0, 0.0};
+   const std::vector<float> leftShoulderSeed_ = {-0.23, -0.07, 0.75, -1.53, 1.21, -0.40, 0.0};
    /*Side Grip*/
 //   const std::vector<float> leftShoulderSeed_ = {-0.04, -0.24, 0.49, -1.30, 0.71, 0.61, -0.24};
-   const std::vector<float> rightShoulderSeed_ = {-0.23, 0.72, 0.65, 1.51, 2.77, 0.0, 0.0};
+   const std::vector<float> rightShoulderSeed_ = {-0.81, -0.15, 1.65, 1.42, 1.28, 0.0, -0.23};
 
 };
 
