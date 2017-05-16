b27c2a8d5ebfd550a5de9c6564d3abf10169c37a
shlok <ssagarwal@wpi.edu>
2017-05-16 16:55:04 -0400

    afterPanelGraspPose function added in task2 utils.

--- val_tasks/val_task2/include/val_task2/val_task2_utils.h
+++ val_tasks/val_task2/include/val_task2/val_task2_utils.h
@@ -7,26 +7,38 @@
 #include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
 #include <val_common/val_common_names.h>
+#include <val_control/val_chest_navigation.h>
+#include <val_control/val_pelvis_navigation.h>
+#include <val_control/val_gripper_control.h>
+#include <val_control/val_head_navigation.h>
+#include <val_control/val_arm_navigation.h>
 
 class task2Utils {
 private:
     ros::NodeHandle nh_;
-    ros::Subscriber satellite_sub_;
-    srcsim::Satellite msg_;
+    // chest controller
+    chestTrajectory* chest_controller_;
+    //pelvis controller
+    pelvisTrajectory* pelvis_controller_;
+    //head controller
+    HeadTrajectory* head_controller_;
+    //grippers
+    gripperControl* gripper_controller_;
+    // arm
+    armTrajectory* arm_controller_;
 
-    ros::Publisher marker_pub_;
-    void satelliteMsgCB (const srcsim::Satellite &msg);
+    // Need to edit
+    const std::vector<float> leftShoulderSeedPanelGraspStatic_ = {-0.23, -0.07, 0.75, -1.53, 1.21, -0.40, 0.0};
+    const std::vector<float> leftShoulderSeedPanelGraspWalk_ = {-0.23, -0.07, 0.75, -1.53, 1.21, -0.40, 0.0};
+
+    //before walking
+    const std::vector<float> rightShoulderSeedPanelGraspStatic_ = {-0.04, 1.04, 0.17, 0.94, 1.18, 0.01, -0.08};
+    //while walking
+    const std::vector<float> rightShoulderSeedPanelGraspWalk_ = {-0.04, 1.16, 0.12, 0.94, 1.13, 0.01, -0.08};
 
 public:
     task2Utils(ros::NodeHandle nh);
+    void afterPanelGraspPose(const armSide side);
     ~task2Utils();
 
-    bool isPitchCorrectNow(void);
-    bool isYawCorrectNow(void);
-    bool isPitchCompleted(void);
-    bool isYawCompleted(void);
-    double getPitchDiff (void);
-    double getYawDiff (void);
-    void getCircle3D (geometry_msgs::Point center, geometry_msgs::Point start,geometry_msgs::Pose pose, const std::vector<float> planeCoeffs, std::vector<geometry_msgs::Pose> &points, float radius, int steps =10);
-    void visulatise6DPoints(std::vector<geometry_msgs::Pose> &points);
 };
--- val_tasks/val_task2/src/val_task2_utils.cpp
+++ val_tasks/val_task2/src/val_task2_utils.cpp
@@ -1,18 +1,15 @@
 #include <val_task2/val_task2_utils.h>
-#include <val_control/val_chest_navigation.h>
-#include <val_control/val_pelvis_navigation.h>
-#include <val_control/val_gripper_control.h>
-#include <val_control/val_head_navigation.h>
-#include <val_control/val_arm_navigation.h>
+
 
 task2Utils::task2Utils(ros::NodeHandle nh):
     nh_(nh)
 {
-    // subscriber for the satellite message
-    satellite_sub_ = nh_.subscribe("/task1/checkpoint2/satellite", 10, &task2Utils::satelliteMsgCB, this);
-
-    // marker publisher
-    marker_pub_ = nh_.advertise<visualization_msgs::MarkerArray>( "handle_path", 10, true);
+    // controllers
+    chest_controller_    = new chestTrajectory(nh_);
+    pelvis_controller_   = new pelvisTrajectory(nh_);
+    head_controller_     = new HeadTrajectory(nh_);
+    gripper_controller_  = new gripperControl(nh_);
+    arm_controller_      = new armTrajectory(nh_);
 }
 
 task2Utils::~task2Utils()
@@ -20,156 +17,32 @@ task2Utils::~task2Utils()
 
 }
 
-
-void task2Utils::satelliteMsgCB(const srcsim::Satellite& msg)
+void task2Utils::afterPanelGraspPose(const armSide side)
 {
-    // update the message
-    msg_ = msg;
-}
+    chest_controller_->controlChest(0,0,0);
+    ros::Duration(2).sleep();
+    const std::vector<float> *seed1,*seed2;
+    if(side == armSide::LEFT){
+        seed1 = &leftShoulderSeedPanelGraspStatic_;
+        seed2 = &leftShoulderSeedPanelGraspWalk_;
 
-// satellite dish helper tasks
-bool task2Utils::isPitchCorrectNow(void)
-{
-    return msg_.pitch_correct_now;
-}
-
-bool task2Utils::isYawCorrectNow(void)
-{
-    return msg_.yaw_correct_now;
-}
-
-bool task2Utils::isPitchCompleted(void)
-{
-    return msg_.pitch_completed;
-}
-
-bool task2Utils::isYawCompleted(void)
-{
-    return msg_.yaw_completed;
-}
-
-double task2Utils::getPitchDiff (void)
-{
-    return (msg_.target_pitch - msg_.current_pitch);
-}
-
-double task2Utils::getYawDiff (void)
-{
-    return (msg_.target_yaw - msg_.current_yaw);
-}
-
-void task2Utils::getCircle3D(geometry_msgs::Point center, geometry_msgs::Point start, geometry_msgs::Pose pose, const std::vector<float> planeCoeffs, std::vector<geometry_msgs::Pose> &points, float radius, int steps)
-{
-    if (planeCoeffs.size() != 4){
-        ROS_INFO("Please check the plane coefficiants");
     }
-
-    // plane co-effecients
-    float a = planeCoeffs.at(0);
-    float b = planeCoeffs.at(1);
-    float c = planeCoeffs.at(2);
-    float d = planeCoeffs.at(3);
-
-    // starting point
-    geometry_msgs::Pose circle_point_pose;
-    circle_point_pose.position.x = start.x;
-    circle_point_pose.position.y = start.y;
-    circle_point_pose.position.z = start.z;
-    circle_point_pose.orientation = pose.orientation;
-    points.push_back(circle_point_pose);
-
-    float dist = fabs(a*start.x  + b*start.y + c*start.z  + d )/sqrt(pow(a,2) + pow(b,2) + pow(c,2));
-
-    geometry_msgs::Pose start_pose;
-    start_pose.position.x = start.x;
-    start_pose.position.y = start.z;
-    start_pose.position.y = start.z;
-
-    for (int i=0;i<steps; i++)
+    else
     {
-        // angle to the first point
-        float alpha = atan2((start_pose.position.y - center.y),(start_pose.position.x - center.x));
-        circle_point_pose.position.x = center.x + radius*cos(alpha - (float)(2*M_PI/steps));
-        circle_point_pose.position.y = center.y + radius*sin(alpha - (float)(2*M_PI/steps));
-
-        //point.position.z = -(a*point.position.x  + b* point.position.y + d)/c;
-        circle_point_pose.position.z = -(a*circle_point_pose.position.x  + b* circle_point_pose.position.y + + d)/c + dist; //(d - dist+.05) )/c;
-
-        // orientation
-        circle_point_pose.orientation = pose.orientation;
-        points.push_back(circle_point_pose);
-
-        start_pose = circle_point_pose;
-    }
-}
-
-void task2Utils::visulatise6DPoints(std::vector<geometry_msgs::Pose> &points)
-{
-    // visulation of the circle
-    visualization_msgs::MarkerArray marker_array = visualization_msgs::MarkerArray();
-
-    visualization_msgs::Marker marker;
-    marker.header.frame_id = VAL_COMMON_NAMES::WORLD_TF;
-    marker.header.stamp = ros::Time();
-    marker.ns = "circle";
-    marker.id = 0;
-    marker.type = visualization_msgs::Marker::ARROW;
-    marker.action = visualization_msgs::Marker::ADD;
-    marker.pose = points[0];
-    marker.scale.x = 0.1;
-    marker.scale.y = 0.01;
-    marker.scale.z = 0.01;
-    marker.color.a = 0.6;
-    marker.color.r = 1.0;
-    marker.color.g = 0.0;
-    marker.color.b = 0.0;
-    marker.lifetime = ros::Duration(0);
-    marker_array.markers.push_back(marker);
-
-    for (int i = 1; i < points.size(); i++) {
-        visualization_msgs::Marker marker_tangent;
-        marker_tangent.header.frame_id = VAL_COMMON_NAMES::WORLD_TF;
-        marker_tangent.header.stamp = ros::Time();
-        marker_tangent.ns = "circle";
-        marker_tangent.id = i;
-        marker_tangent.type = visualization_msgs::Marker::ARROW;
-        marker_tangent.action = visualization_msgs::Marker::ADD;
-        marker_tangent.pose = points[i];
-
-//        ROS_INFO_STREAM(i<<"pose"<<points[i].position);
-        marker_tangent.scale.x = 0.05;
-        marker_tangent.scale.y = 0.006;
-        marker_tangent.scale.z = 0.006;
-        marker_tangent.color.a = 0.6;
-        marker_tangent.color.r = ((i==1) ? 1.0: 0.0);
-        marker_tangent.color.g = ((i==1) ? 0.0: 1.0);
-        marker_tangent.color.b = 0.0;
-        marker_tangent.lifetime = ros::Duration(0);
-        marker_array.markers.push_back(marker_tangent);
-
-        visualization_msgs::Marker marker_point;
-        marker_point.header.frame_id = VAL_COMMON_NAMES::WORLD_TF;
-        marker_point.header.stamp = ros::Time();
-        marker_point.ns = "circle";
-        marker_point.id = i+points.size();
-        marker_point.type = visualization_msgs::Marker::CUBE;
-        marker_point.action = visualization_msgs::Marker::ADD;
-        marker_point.pose = points[i];
-
-//        ROS_INFO_STREAM(i<<"pose"<<points[i].position);
-        marker_point.scale.x = 0.005;
-        marker_point.scale.y = 0.005;
-        marker_point.scale.z = 0.005;
-        marker_point.color.a = 0.6;
-        marker_point.color.r = 0.0;
-        marker_point.color.g = .0;
-        marker_point.color.b = 1.0;
-        marker_point.lifetime = ros::Duration(0);
-        marker_array.markers.push_back(marker_point);
+        seed1 = &rightShoulderSeedPanelGraspStatic_;
+        seed2 = &rightShoulderSeedPanelGraspWalk_;
 
     }
+    std::vector< std::vector<float> > armData;
+    armData.push_back(*seed1);
 
-    marker_pub_.publish(marker_array);
-    ros::Duration(0.2).sleep();
+    arm_controller_->moveArmJoints(side, armData, 2.0f);
+    ros::Duration(2).sleep();
+
+    armData.clear();
+    armData.push_back(*seed2);
+
+    arm_controller_->moveArmJoints(side, armData, 2.0f);
+    ros::Duration(2).sleep();
 }
 
