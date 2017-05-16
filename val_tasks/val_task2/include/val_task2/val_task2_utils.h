47924e4db0c9bf7ac844abf59c7d9ed5f3a8bea0
Vinayak Jagtap <vinayak.jagtap04@gmail.com>
2017-05-16 15:43:08 -0400

    Added missing task2 utils file

---
+++ val_tasks/val_task2/include/val_task2/val_task2_utils.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <ros/ros.h>
+#include <srcsim/Satellite.h>
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/Pose.h>
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <val_common/val_common_names.h>
+
+class task2Utils {
+private:
+    ros::NodeHandle nh_;
+    ros::Subscriber satellite_sub_;
+    srcsim::Satellite msg_;
+
+    ros::Publisher marker_pub_;
+    void satelliteMsgCB (const srcsim::Satellite &msg);
+
+public:
+    task2Utils(ros::NodeHandle nh);
+    ~task2Utils();
+
+    bool isPitchCorrectNow(void);
+    bool isYawCorrectNow(void);
+    bool isPitchCompleted(void);
+    bool isYawCompleted(void);
+    double getPitchDiff (void);
+    double getYawDiff (void);
+    void getCircle3D (geometry_msgs::Point center, geometry_msgs::Point start,geometry_msgs::Pose pose, const std::vector<float> planeCoeffs, std::vector<geometry_msgs::Pose> &points, float radius, int steps =10);
+    void visulatise6DPoints(std::vector<geometry_msgs::Pose> &points);
+};
