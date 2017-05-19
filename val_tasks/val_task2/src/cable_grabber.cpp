de6175a622eb4f330d5aa94c3a3f2cc5560a1453
shlok <ssagarwal@wpi.edu>
2017-05-19 14:51:01 -0400

    added a new state for gripper. made few alterations to cable grabber code. still not grabbing the cable completely

--- val_tasks/val_task2/src/cable_grabber.cpp
+++ val_tasks/val_task2/src/cable_grabber.cpp
@@ -11,25 +11,32 @@ cableGrabber::cableGrabber(ros::NodeHandle n):nh_(n), armTraj_(nh_), gripper_(nh
     leftHandOrientation_.quaternion.z = -0.583;
     leftHandOrientation_.quaternion.w = 0.326;
 
-//    /* Side Grip */
-//    leftHandOrientation_.quaternion.x = 0.155;
-//    leftHandOrientation_.quaternion.y = -0.061;
-//    leftHandOrientation_.quaternion.z = -0.696;
-//    leftHandOrientation_.quaternion.w = 0.699;
+    //    /* Side Grip */
+    //    leftHandOrientation_.quaternion.x = 0.155;
+    //    leftHandOrientation_.quaternion.y = -0.061;
+    //    leftHandOrientation_.quaternion.z = -0.696;
+    //    leftHandOrientation_.quaternion.w = 0.699;
 
     //    /* Side Grip */
-//    rightHandOrientation_.header.frame_id = VAL_COMMON_NAMES::PELVIS_TF;
-//    rightHandOrientation_.quaternion.x = -0.094;
-//    rightHandOrientation_.quaternion.y = -0.027;
-//    rightHandOrientation_.quaternion.z = 0.973;
-//    rightHandOrientation_.quaternion.w = -0.209;
+    //    rightHandOrientation_.header.frame_id = VAL_COMMON_NAMES::PELVIS_TF;
+    //    rightHandOrientation_.quaternion.x = -0.094;
+    //    rightHandOrientation_.quaternion.y = -0.027;
+    //    rightHandOrientation_.quaternion.z = 0.973;
+    //    rightHandOrientation_.quaternion.w = -0.209;
 
-    /* Top Grip */
+    /* Top Grip Flat Hand */
+    //    rightHandOrientation_.header.frame_id = VAL_COMMON_NAMES::PELVIS_TF;
+    //    rightHandOrientation_.quaternion.x = -0.576;
+    //    rightHandOrientation_.quaternion.y = 0.397;
+    //    rightHandOrientation_.quaternion.z = 0.632;
+    //    rightHandOrientation_.quaternion.w = 0.332;
+
+    /* Top Grip Slightly Bent Hand */
     rightHandOrientation_.header.frame_id = VAL_COMMON_NAMES::PELVIS_TF;
-    rightHandOrientation_.quaternion.x = -0.576;
-    rightHandOrientation_.quaternion.y = 0.397;
-    rightHandOrientation_.quaternion.z = 0.632;
-    rightHandOrientation_.quaternion.w = 0.332;
+    rightHandOrientation_.quaternion.x = 0.640;
+    rightHandOrientation_.quaternion.y = -0.380;
+    rightHandOrientation_.quaternion.z = -0.614;
+    rightHandOrientation_.quaternion.w = -0.261;
 
     // cartesian planners for the arm
     left_arm_planner_ = new cartesianPlanner("leftPalm", VAL_COMMON_NAMES::WORLD_TF);
@@ -58,7 +65,7 @@ void cableGrabber::grasp_cable(const armSide side, const geometry_msgs::Point &g
         finalOrientationStamped = &rightHandOrientation_;
     }
 
-
+    // opening gripper
     ROS_INFO("opening grippers");
     gripper_.openGripper(side);
 
@@ -70,47 +77,44 @@ void cableGrabber::grasp_cable(const armSide side, const geometry_msgs::Point &g
     armTraj_.moveArmJoints(side, armData, executionTime);
     ros::Duration(executionTime*2).sleep();
 
-
-    //move arm to given point with known orientation and higher z
-    geometry_msgs::Pose finalGoal, intermGoal;
-    geometry_msgs::Point finalPoint, intermPoint;
-
-    current_state_->transformPoint(goal,intermPoint, VAL_COMMON_NAMES::WORLD_TF, VAL_COMMON_NAMES::PELVIS_TF);
-    intermPoint.z += 0.1;
-
-    //transform that point back to world frame
-    current_state_->transformPoint(intermPoint, intermPoint, VAL_COMMON_NAMES::PELVIS_TF, VAL_COMMON_NAMES::WORLD_TF);
-
-    ROS_INFO("Moving at an intermidate point before goal");
-    intermGoal.position = intermPoint;
+    geometry_msgs::Pose finalGoal;
     geometry_msgs::QuaternionStamped temp  = *finalOrientationStamped;
-
     current_state_->transformQuaternion(temp, temp);
-    intermGoal.orientation = temp.quaternion;
 
-    armTraj_.moveArmInTaskSpace(side, intermGoal, executionTime*2);
-    ros::Duration(executionTime*2).sleep();
-
-    //move arm to final position with known orientation
-
-    current_state_->transformPoint(goal,finalPoint, VAL_COMMON_NAMES::WORLD_TF, VAL_COMMON_NAMES::PELVIS_TF);
-    finalPoint.x -= 0.04; // this is to compensate for the distance between palm frame and center of palm
-
-    //transform that point back to world frame
-    current_state_->transformPoint(finalPoint, finalPoint, VAL_COMMON_NAMES::PELVIS_TF, VAL_COMMON_NAMES::WORLD_TF);
-
-    ROS_INFO("Moving towards goal");
-    finalGoal.position = finalPoint;
+    ROS_INFO("Moving towards goal with whole body controller");
+    finalGoal.position = goal;
+    finalGoal.position.z+=0.05;
     finalGoal.orientation = temp.quaternion;
 
     std::vector<geometry_msgs::Pose> waypoints;
-
     waypoints.push_back(finalGoal);
 
     moveit_msgs::RobotTrajectory traj;
     right_arm_planner_->getTrajFromCartPoints(waypoints, traj, false);
     wholebody_controller_->compileMsg(side, traj.joint_trajectory);
 
+    gripper_.controlGripper(side,GRIPPER_STATE::OPEN_THUMB_IN_APPROACH);
+    ros::Duration(0.3).sleep();
+
+    std::vector<double> grip1 ={1.3999, 0.3, 0.5, 0.0, 0.0};
+    std::vector<double> grip2 ={1.3999, 0.5, 0.5, 0.0, 0.0};
+    std::vector<double> grip3 ={1.3999, 0.7, 0.6, 0.0, 0.0};
+    std::vector<double> grip4 ={1.3999, 0.9, 0.7, 0.0, 0.0};
+
+    gripper_.controlGripper(side,grip1);
+    ros::Duration(0.3).sleep();
+    gripper_.controlGripper(side,grip2);
+    ros::Duration(0.3).sleep();
+    gripper_.controlGripper(side,grip3);
+    ros::Duration(0.3).sleep();
+    gripper_.controlGripper(side,grip4);
+    ros::Duration(0.3).sleep();
+
+    // nudging arm forward
+    ros::Duration(0.3).sleep();
+    armTraj_.nudgeArmLocal(side,direction::FRONT);
+
+    // closing grippers
     ros::Duration(executionTime).sleep();
     ROS_INFO("Closing grippers");
     gripper_.closeGripper(side);
