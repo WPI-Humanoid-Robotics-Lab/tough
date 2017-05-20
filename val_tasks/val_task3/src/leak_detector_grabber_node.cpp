8ce88f0b49a8775b12d3b9479ef0950b4719bc83
Your Name <you@example.com>
2017-05-20 13:08:07 -0400

    Cable detector modified unsaved changes from leakdetector grasping node

--- val_tasks/val_task3/src/leak_detector_grabber_node.cpp
+++ val_tasks/val_task3/src/leak_detector_grabber_node.cpp
@@ -1,19 +1,41 @@
 #include <val_task3/leak_detector_grabber.h>
+#include <val_control/robot_state.h>
 
 int main(int argc, char **argv){
 
     ros::init(argc, argv, "leak_detector_grabber");
     ros::NodeHandle nh;
     leakDetectorGrabber ldg(nh);
+    RobotStateInformer* rs = RobotStateInformer::getRobotStateInformer(nh);
 
     ROS_INFO("starting leak detector grabber");
 
     geometry_msgs::Pose goal;
-    goal.position.x = 0.0851;
-    goal.position.y = -0.811;
-    goal.position.z = 0.97;
-    goal.orientation.w = 1.0;
-    ldg.graspDetector(goal);
+    geometry_msgs::Pose fGoal;
+
+    goal.orientation.x = -0.504;
+    goal.orientation.y = 0.535;
+    goal.orientation.z = 0.504;
+    goal.orientation.w = 0.453;
+
+    rs->transformPose(goal,fGoal,VAL_COMMON_NAMES::PELVIS_TF);
+
+
+    if(argc == 4){
+
+        fGoal.position.x = std::atof(argv[1]);
+        fGoal.position.y = std::atof(argv[2]);
+        fGoal.position.z = std::atof(argv[3]);
+    }
+
+    else{
+
+        fGoal.position.x = -0.445;
+        fGoal.position.y = 0.159;
+        fGoal.position.z = 0.927;
+    }
+
+    ldg.graspDetector(fGoal);
 
     ros::spin();
     return 0;
