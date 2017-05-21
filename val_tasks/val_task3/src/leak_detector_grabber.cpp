d66f44da486a1c1e7aee69d5965d3b74a649f2c6
muddysahil <skejriwal@wpi.edu>
2017-05-21 17:15:26 -0400

    Leak Detector grab

--- val_tasks/val_task3/src/leak_detector_grabber.cpp
+++ val_tasks/val_task3/src/leak_detector_grabber.cpp
@@ -7,49 +7,122 @@ leakDetectorGrabber::leakDetectorGrabber(ros::NodeHandle nh):nh_(nh),
               armTraj_(nh_), gripper_(nh_), wholebody_controller_(nh_)
               {
     right_arm_planner_ = new cartesianPlanner("rightPalm");
+    left_arm_planner_ = new cartesianPlanner("leftPalm");
     current_state_ = RobotStateInformer::getRobotStateInformer(nh_);
-//    right_arm_planner_ = cartesianPlanner("rightpalm", VAL_COMMON_NAMES::WORLD_TF);
-//    left_arm_planner_ = cartesianPlanner("leftpalm", VAL_COMMON_NAMES::WORLD_TF);
 }
 
 leakDetectorGrabber::~leakDetectorGrabber(){
 
 }
 
-void leakDetectorGrabber::graspDetector(armSide side, const geometry_msgs::Pose &goal, float executionTime){
 
+void leakDetectorGrabber::graspDetector(armSide side, geometry_msgs::Pose &goal, float executionTime){
 
+    std::string palmFrame;
+    float palmToFingerOffset;
+
+    if(side == armSide::LEFT){
+        palmFrame = VAL_COMMON_NAMES::L_END_EFFECTOR_FRAME;
+        palmToFingerOffset = -0.04;
+    }
+    else {
+        palmFrame = VAL_COMMON_NAMES::R_END_EFFECTOR_FRAME;
+        palmToFingerOffset = 0.04;
+    }
+
+    ROS_INFO("opening grippers");
+    gripper_.openGripper(side);
+
+    //move arm to given point with known orientation and higher z
     geometry_msgs::Pose finalGoal, intermGoal;
-    std::vector<geometry_msgs::Pose> waypoints;
-    moveit_msgs::RobotTrajectory traj;
 
     current_state_->transformPose(goal,intermGoal, VAL_COMMON_NAMES::WORLD_TF, VAL_COMMON_NAMES::PELVIS_TF);
     intermGoal.position.z += 0.1;
+
+    //transform that point back to world frame
     current_state_->transformPose(intermGoal, intermGoal, VAL_COMMON_NAMES::PELVIS_TF, VAL_COMMON_NAMES::WORLD_TF);
+    taskCommonUtils::fixHandFramePose(nh_, side, intermGoal);
 
     ROS_INFO("Moving at an intermidate point before goal");
+    ROS_INFO_STREAM("Intermidiate goal"<<intermGoal);
     armTraj_.moveArmInTaskSpace(side, intermGoal, executionTime*2);
     ros::Duration(executionTime*2).sleep();
 
-    current_state_->transformPose(goal,finalGoal, VAL_COMMON_NAMES::WORLD_TF, VAL_COMMON_NAMES::PELVIS_TF);
-    finalGoal.position.x -= 0.04;
-    finalGoal.position.y += 0.01;
-    finalGoal.position.z += 0.002;
-    current_state_->transformPose(finalGoal, finalGoal, VAL_COMMON_NAMES::PELVIS_TF, VAL_COMMON_NAMES::WORLD_TF);
+    //move arm to final position with known orientation
 
-    ROS_INFO("Moving towards goal");
+    current_state_->transformPose(goal,finalGoal, VAL_COMMON_NAMES::WORLD_TF, palmFrame);
+    current_state_->transformPose(intermGoal,intermGoal, VAL_COMMON_NAMES::WORLD_TF, palmFrame);
 
-    waypoints.push_back(finalGoal);
-    ROS_INFO_STREAM(finalGoal);
-    right_arm_planner_->getTrajFromCartPoints(waypoints, traj, false);
+    intermGoal.position.y += palmToFingerOffset;
+    intermGoal.position.z -= 0.02; //finger to center of palm in Z-axis of hand frame
+    finalGoal.position.y  += palmToFingerOffset; // this is to compensate for the distance between palm frame and center of palm
+    finalGoal.position.z  -= 0.02; //finger to center of palm in Z-axis of hand frame
 
-    ROS_INFO("Calculated Traj");
-    wholebody_controller_.compileMsg(side, traj.joint_trajectory);
+    //transform that point back to world frame
+        current_state_->transformPose(finalGoal, finalGoal, palmFrame, VAL_COMMON_NAMES::WORLD_TF);
+        current_state_->transformPose(intermGoal, intermGoal, palmFrame, VAL_COMMON_NAMES::WORLD_TF);
+        taskCommonUtils::fixHandFramePose(nh_, side, finalGoal);
 
-    ros::Duration(executionTime).sleep();
-    ROS_INFO("Closing grippers");
-    ros::Duration(0.3).sleep();
-    gripper_.closeGripper(side);
-    ros::Duration(0.3).sleep();
+        ROS_INFO("Moving towards goal");
+        ROS_INFO_STREAM("Final goal"<<finalGoal);
+        std::vector<geometry_msgs::Pose> waypoints;
+
+        waypoints.push_back(intermGoal);
+        waypoints.push_back(finalGoal);
+
+        ROS_INFO_STREAM(finalGoal);
+        moveit_msgs::RobotTrajectory traj;
+
+        if(side == armSide::LEFT){
+
+            left_arm_planner_->getTrajFromCartPoints(waypoints, traj, false);
+        }
+        else{
+
+            right_arm_planner_->getTrajFromCartPoints(waypoints, traj, false);
+        }
+
+        ROS_INFO("Calculated Traj");
+        wholebody_controller_.compileMsg(side, traj.joint_trajectory);
+
+        ros::Duration(executionTime).sleep();
+        ROS_INFO("Closing grippers");
+        gripper_.closeGripper(side);
+        ros::Duration(0.3).sleep();
+
+//    geometry_msgs::Pose finalGoal, intermGoal;
+//    std::vector<geometry_msgs::Pose> waypoints;
+//    moveit_msgs::RobotTrajectory traj;
+
+//    taskCommonUtils::fixHandFramePose(nh,side,goal);
+
+//    current_state_->transformPose(goal,intermGoal, VAL_COMMON_NAMES::WORLD_TF, VAL_COMMON_NAMES::PELVIS_TF);
+//    intermGoal.position.z += 0.1;
+//    current_state_->transformPose(intermGoal, intermGoal, VAL_COMMON_NAMES::PELVIS_TF, VAL_COMMON_NAMES::WORLD_TF);
+
+//    ROS_INFO("Moving at an intermidate point before goal");
+//    armTraj_.moveArmInTaskSpace(side, intermGoal, executionTime*2);
+//    ros::Duration(executionTime*2).sleep();
+
+//    current_state_->transformPose(goal,finalGoal, VAL_COMMON_NAMES::WORLD_TF, VAL_COMMON_NAMES::PELVIS_TF);
+//   // finalGoal.position.x -= 0.04;
+//   // finalGoal.position.y += 0.01;
+//    finalGoal.position.z += 0.002;
+//    current_state_->transformPose(finalGoal, finalGoal, VAL_COMMON_NAMES::PELVIS_TF, VAL_COMMON_NAMES::WORLD_TF);
+
+//    ROS_INFO("Moving towards goal");
+
+//    waypoints.push_back(finalGoal);
+//    ROS_INFO_STREAM(finalGoal);
+//    right_arm_planner_->getTrajFromCartPoints(waypoints, traj, false);
+
+//    ROS_INFO("Calculated Traj");
+//    wholebody_controller_.compileMsg(side, traj.joint_trajectory);
+
+//    ros::Duration(executionTime).sleep();
+//    ROS_INFO("Closing grippers");
+//    ros::Duration(0.3).sleep();
+//    gripper_.closeGripper(side);
+//    ros::Duration(0.3).sleep();
 }
 
--- val_tasks/val_task3/src/leak_detector_grabber_node.cpp
+++ val_tasks/val_task3/src/leak_detector_grabber_node.cpp
@@ -1,33 +1,16 @@
 #include <val_task3/leak_detector_grabber.h>
 #include <val_control/robot_state.h>
-#include <val_task_common/val_task_common_utils.h>
-#include <tf/transform_datatypes.h>
+
 
 int main(int argc, char **argv){
 
     ros::init(argc, argv, "leak_detector_grabber");
     ros::NodeHandle nh;
     leakDetectorGrabber ldg(nh);
-    RobotStateInformer* rs = RobotStateInformer::getRobotStateInformer(nh);
 
     ROS_INFO("starting leak detector grabber");
 
     geometry_msgs::Pose goal;
-    geometry_msgs::Pose fGoal;
-    goal.orientation.x = std::atof(argv[4]);
-    goal.orientation.y = std::atof(argv[5]);
-    goal.orientation.z = std::atof(argv[6]);
-    goal.orientation.w = std::atof(argv[7]);
-
-    taskCommonUtils::fixHandFramePose(nh,RIGHT,goal);
-
-//    goal.orientation.x = -0.504;
-//    goal.orientation.y = 0.535;
-//    goal.orientation.z = 0.504;
-//    goal.orientation.w = 0.453;
-
-//    rs->transformPose(goal,fGoal,VAL_COMMON_NAMES::PELVIS_TF);
-
 
     if(argc == 8){
 
@@ -42,6 +25,12 @@ int main(int argc, char **argv){
         goal.position.y = 0.159;
         goal.position.z = 0.927;
     }
+
+
+    goal.orientation.x = std::atof(argv[4]);
+    goal.orientation.y = std::atof(argv[5]);
+    goal.orientation.z = std::atof(argv[6]);
+    goal.orientation.w = std::atof(argv[7]);
 
     ldg.graspDetector(RIGHT,goal);
 
