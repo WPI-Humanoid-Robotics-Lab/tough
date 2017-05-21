dbe8c70479533bbd58b35a134f5d4bde05441f6e
Vinayak Jagtap <vinayak.jagtap04@gmail.com>
2017-05-20 20:59:00 -0400

    disabled auto purging of pointcloud. might fix issue #37

--- val_perception/perception_common/src/periodic_snapshotter.cpp
+++ val_perception/perception_common/src/periodic_snapshotter.cpp
@@ -299,7 +299,7 @@ void PeriodicSnapshotter::mergeClouds(const sensor_msgs::PointCloud2::Ptr msg){
     pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_msg(new pcl::PointCloud<pcl::PointXYZ>);
     convertROStoPCL(msg, pcl_msg);
 
-    if (state_request == PCL_STATE_CONTROL::RESET || prev_msg_->row_step > 10000000 || prev_msg_->data.empty()){
+    if (state_request == PCL_STATE_CONTROL::RESET || prev_msg_->data.empty()){
         ROS_INFO("Resetting Pointcloud");
         merged_cloud = msg;
         pointcloud_for_octomap_pub_.publish(prev_msg_->data.empty() ? msg : prev_msg_);
