3ff16a3a334d0701337a2a0598f3b817df7c3d77
Vinayak Jagtap <vinayak.jagtap04@gmail.com>
2017-05-21 22:10:23 -0400

    added return capability for handle detection

--- val_tasks/val_task1/src/pcl_handle_detector_node.cpp
+++ val_tasks/val_task1/src/pcl_handle_detector_node.cpp
@@ -5,20 +5,27 @@ int main(int argc, char** argv)
     ros::init (argc,argv,"findpclHandleDetector");
     ros::NodeHandle nh;
     geometry_msgs::Pose panel_loc;
-    panel_loc.position.x =  2.35336247755;
-    panel_loc.position.y = -0.13536594331;
+    panel_loc.position.x =  3.06;
+    panel_loc.position.y = 0.255;
     panel_loc.position.z = 0;
-    panel_loc.orientation.x = 0;
-    panel_loc.orientation.y = 0;
-    panel_loc.orientation.z = -0.69335672234;
-    panel_loc.orientation.w = 0.720594515373;
+    tf::Quaternion q = tf::createQuaternionFromYaw(M_PI_4);
+    tf::quaternionTFToMsg(q, panel_loc.orientation);
+//    panel_loc.orientation.x = 0;
+//    panel_loc.orientation.y = 0;
+//    panel_loc.orientation.z = -0.69335672234;
+//    panel_loc.orientation.w = 0.720594515373;
 
-    float offset = 1.1;
-
-    pcl_handle_detector  obj(nh,offset,panel_loc);
+    ros::Rate loop(1);
+    pcl_handle_detector  obj(nh,panel_loc);
     while(ros::ok())
     {
-        ros::spin();
+        std::vector<geometry_msgs::Point> handle_locations;
+        obj.getDetections(handle_locations);
+        for (auto location : handle_locations){
+            ROS_INFO("X: %.2f Y: %.2f Z: %.2f", location.x, location.y, location.z);
+        }
+        ros::spinOnce();
+        loop.sleep();
     }
 
     return 0;
