ed941cea4193c2495cd260e6ed52e8e24f588e33
jadhavan <ajayabalan@wpi.edu>
2017-06-02 00:24:54 -0400

    modified script, seems to be working fine, be aware of the size of the logs

--- val_common/scripts/runTask1.sh
+++ val_common/scripts/runTask1.sh
@@ -12,8 +12,8 @@ while true
 do
   current_time=$(date "+%Y.%m.%d-%H.%M.%S")
   cd $HOME
-  if [ ! -d /indigo_ws/src/space_robotics_challenge/val_tasks/val_task1/log ]; then
-    mkdir -p /indigo_ws/src/space_robotics_challenge/val_tasks/val_task1/log;
+  if [ ! -d ~/indigo_ws/src/space_robotics_challenge/val_tasks/val_task1/log ]; then
+    mkdir -p ~/indigo_ws/src/space_robotics_challenge/val_tasks/val_task1/log;
   fi
 
   data_log=$HOME'/indigo_ws/src/space_robotics_challenge/val_tasks/val_task1/log/whrlT1_'$current_time'.log'
@@ -31,7 +31,7 @@ do
   echo -e "\e[32mINFO:\e[0m Launching SRCSIM"
   roslaunch srcsim unique_task1.launch init:=true &
 
-  sleep 350
+  sleep 150
   
   roslaunch val_bringup whrl_task1.launch --screen > $data_log &
   # stdbuf -oL roslaunch val_bringup whrl_task1.launch --screen &> $data_log
@@ -41,13 +41,13 @@ do
 
   echo -e "\e[32mINFO:\e[0m Start Gazebo Log"
   gz log -d 1
-  echo -e "\e[32mINFO:\e[0m Launching Task2"
+  echo -e "\e[32mINFO:\e[0m Launching Task1"
   
 
   roslaunch val_task1 task1.launch --screen > $data_log2 &
   # stdbuf -oL roslaunch val_task2 task2.launch --screen --screen &> $data_log2
 
-  sleep 1200
+  sleep 2000
 
   echo -e "\e[32mINFO:\e[0m Stop Gazebo Log"
   gz log -d 0
