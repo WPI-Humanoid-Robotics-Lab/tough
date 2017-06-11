fff0761d056e48e5dfd62a265cc5aedc8913903e
Your Name <you@example.com>
2017-06-11 19:44:23 -0400

    updated gui

--- val_bringup/scripts/gui.py
+++ val_bringup/scripts/gui.py
@@ -3,9 +3,10 @@
 import sys
 import socket
 import subprocess
+import select
 import rospy
 from PyQt4 import QtCore, QtGui, uic
- 
+
 qtCreatorFile = "rossrc.ui"
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
@@ -14,43 +15,24 @@ Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 FIELD_IP = "localhost"
 PORT = 8080
 
-class mysocket:
-    def __init__(self, sock=None):
-        if sock is None:
-            self.sock = socket.socket(
-                socket.AF_INET, socket.SOCK_STREAM)
-        else:
-            self.sock = sock
+host = FIELD_IP
+port = PORT
 
-    def connect(self, host, port):
-        self.sock.connect((host, port))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(2)
 
-    def mysend(self, msg):
-        totalsent = 0
-        while totalsent < len(msg):
-            sent = self.sock.send(msg[totalsent:])
-            if sent == 0:
-                raise RuntimeError("socket connection broken")
-            totalsent = totalsent + sent
+# connect to remote host
+try:
+    s.connect((host, port))
+except:
+    print 'Unable to connect'
+    sys.exit()
 
-    def myreceive(self):
-        chunks = []
-        bytes_recd = 0
-        proceed = True
-        while proceed:
-            chunk = self.sock.recv(4096)
-            if chunk == '':
-                raise RuntimeError("socket connection broken")
-            if (chunk.endswith("\n")):
-                proceed = False
-            chunks.append(chunk)
-            bytes_recd = bytes_recd + len(chunk)
-        return ''.join(chunks)
-
-sock = mysocket()
-sock.connect(FIELD_IP,PORT)
+sys.stdout.write('[OCU] ');
+sys.stdout.flush()
 
 class SrcUI(QtGui.QMainWindow, Ui_MainWindow):
+
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
@@ -61,8 +43,32 @@ class SrcUI(QtGui.QMainWindow, Ui_MainWindow):
         self.walkRotateLineEdit.returnPressed.connect(self.WalkRotate)
         self.walkStepsLineEdit2.returnPressed.connect(self.WalkSteps)
         
+    def SendCommand(self, command):
+        socket_list = [sys.stdin, s]
+        # Get the list sockets which are readable
+        ready_to_read, ready_to_write, in_error = select.select(socket_list, [], [])
+        # print in_error
+        # for sock in ready_to_read:
+        #     print sock 
+        #     print s
+        #     if sock == s:
+        #         # incoming message from remote server, s
+        #         data = sock.recv(4096)
+        #         if not data:
+        #             print '\nDisconnected from braodcast server'
+        #             sys.exit()
+        #         else:
+        #             pass
+        #             # print data
+
+        #     else:
+        #         # user entered a message
+        s.send(command)
+
+
     def Task1(self):
-    	print("Do Task 1")
+        print "Task1\n"
+    	self.SendCommand("+roslaunch val_bringup final1.launch")
 
     def Task2(self):
     	print("Do Task 2")
@@ -73,10 +79,9 @@ class SrcUI(QtGui.QMainWindow, Ui_MainWindow):
     def WalkRotate(self):
     	text = self.walkRotateLineEdit.text()
     	print "**Command Recieved** "
-        print "executing 'runrun val_footstep walk_rotate "+str(text) + "'"
         command = "+runrun val_footstep walk_rotate "+str(text)
-        subprocess.Popen(command.split())
-        # print("rosrun val_footstep walk_rotate "+ text)
+        # command = "+rostopic list"
+        self.SendCommand(command)
     	self.walkRotateLineEdit.clear()
 
     def WalkSteps(self):
