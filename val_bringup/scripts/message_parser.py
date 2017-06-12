5dd36be7b42c8a32d69a093ee3b568ee15092c67
Ben Pinaya <bpinaya@wpi.edu>
2017-06-11 23:27:25 -0400

    added commented code so it doesn't brake anything to test ihmc error

--- val_bringup/scripts/message_parser.py
+++ val_bringup/scripts/message_parser.py
@@ -7,6 +7,7 @@ import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Point
 
+import sched
 
 FIELD_IP = "192.168.2.10"
 # FIELD_IP = "localhost"
@@ -57,13 +58,52 @@ def point_callback(data):
     #send only data that is required. it should be a string only
     sock.mysend(str(time.time()) + " : " + "Clicked point : x:"+str(data.x) +" y:"+ str(data.y) + " z:"+str(data.z) +" \n")
 
+# counter = 0 
+# countercheck = 0 
+# resetflag = False
+
+# def ihmc_callback(data):
+#     print "we get here"
+#     counter = counter + 1
+#     if counter > 5000000 :
+#         counter = 0
+#         resetflag = True
+
+# sch = sched.scheduler(time.time, time.sleep)
+
+# def check_status(sc): 
+#     print "Doing stuff..."
+#     global counter
+#     global countercheck
+#     global resetflag
+#     print countercheck
+#     print counter
+#     print resetflag
+
+#     if counter > countercheck:
+#         print " "
+#     else:
+#         sock.mysend("Probable the IHMC Controller Failed")
+
+#     if resetflag == True:
+#         resetflag = False
+#         countercheck = 0
+
+#     else:
+#         countercheck = counter
+    
+#     sch.enter(5, 1, check_status, (sc,))
+
+# sch.enter(5, 1, check_status, (sch,))
+# sch.run()
+
 def listener():
     # Listen to the decision making topic
     rospy.init_node('state_listener', anonymous=True)
     rospy.Subscriber("/field/log", String, state_callback)
     rospy.Subscriber("/clicked_point", Point, point_callback)
-
-
+    # rospy.Subscriber("/ihmc_ros/valkyrie/output/imu/pelvis_pelvisMiddleImu", String, ihmc_callback)
+    # rospy.Subscriber("/tf", String, ihmc_callback)
 
 if __name__ == '__main__':
     listener()
