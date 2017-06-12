216f6107894982f670207a990b6a3c111e7b7d51
sumanth <sumanth.724@gmail.com>
2017-06-11 18:56:22 -0700

    added leak detector node

---
+++ val_tasks/val_task3/src/leak_detector_node.cpp
@@ -0,0 +1,22 @@
+#include <val_task3/leak_detector.h>
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "leak_detector_node");
+    ros::NodeHandle nh_;
+
+    leakDetector leak_detector(nh_);
+
+    ROS_INFO("generating search points");
+    float v_s = 0.835;
+    float v_e = 1.713;
+
+    geometry_msgs::Point h_s, h_e;
+    h_s.x = 0.2; h_s.y = 0.2; h_s.z = v_e;
+    h_e.x = 0.5; h_e.y = 0.2; h_e.z = v_s;
+
+    std::vector<geometry_msgs::Point> points;
+    leak_detector.generateSearchWayPoints(h_s, h_e, v_s, v_e, points);
+
+    return 0;
+}
