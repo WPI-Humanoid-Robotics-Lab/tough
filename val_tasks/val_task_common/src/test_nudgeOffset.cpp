edf945cc0028c9eb900c2f39f3485e261694ffea
muddysahil <skejriwal@wpi.edu>
2017-06-13 21:04:01 -0400

    Added log for nudge offset

--- val_tasks/val_task_common/src/test_nudgeOffset.cpp
+++ val_tasks/val_task_common/src/test_nudgeOffset.cpp
@@ -3,13 +3,25 @@
 #include "val_moveit_planners/val_cartesian_planner.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <std_msgs/Empty.h>
+#include <std_msgs/String.h>
+
+void taskLog(std::string data, std_msgs::String &publishData){
+
+    publishData.data.clear();
+    publishData.data = data;
+}
+
+
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "test_nudgeOffset");
     ros::NodeHandle nh;
+    std_msgs::String publishData;
     ROS_INFO("Moving the arms");
     armTrajectory armTraj(nh);
+    ros::Publisher taskPB = nh.advertise<std_msgs::String>("/field/log",10);
     wholebodyManipulation wholeBodyController(nh);
     cartesianPlanner right_arm_planner(VAL_COMMON_NAMES::RIGHT_ENDEFFECTOR_GROUP, VAL_COMMON_NAMES::WORLD_TF);
     cartesianPlanner left_arm_planner(VAL_COMMON_NAMES::LEFT_ENDEFFECTOR_GROUP, VAL_COMMON_NAMES::WORLD_TF);
@@ -30,6 +42,8 @@ int main(int argc, char **argv)
         {
             if (right_arm_planner.getTrajFromCartPoints(waypoint, traj, false)> 0.98){
                 ROS_INFO("right arm whole body msg executed");
+                taskLog("test_nudgeOffset : right arm whole body msg executed",publishData);
+                taskPB.publish(publishData);
                 wholeBodyController.compileMsg(side, traj.joint_trajectory);
             }
         }
@@ -37,11 +51,12 @@ int main(int argc, char **argv)
         {
             if (left_arm_planner.getTrajFromCartPoints(waypoint, traj, false)> 0.98){
                 ROS_INFO("left arm whole body msg executed");
+                taskLog("test_nudgeOffset : left arm whole body msg executed",publishData);
+                taskPB.publish(publishData);
                 wholeBodyController.compileMsg(side, traj.joint_trajectory);
             }
 
         }
-
 
     }
     else
