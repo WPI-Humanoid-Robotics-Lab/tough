99dcd8ae47017925d6a9ebfc5c906b4f73599b75
Shlok <ssagarwal@wpi.edu>
2018-01-26 23:57:59 -0500

    added sample unit test. not complete yet.

--- tough_control/tough_controller_interface/src/pelvis_control_interface.cpp
+++ tough_control/tough_controller_interface/src/pelvis_control_interface.cpp
@@ -11,9 +11,12 @@ PelvisControlInterface::~PelvisControlInterface()
     pelvisHeightPublisher_.shutdown();
 }
 
+/**
+ * @brief PelvisControlInterface::controlPelvisHeight controls the height of the pelvis with respect to the ground
+ * @param height is the height in meters
+ */
 void PelvisControlInterface::controlPelvisHeight(float height)
 {
-
     ihmc_msgs::PelvisHeightTrajectoryRosMessage msg;
     ihmc_msgs::EuclideanTrajectoryPointRosMessage p;
 
@@ -31,7 +34,6 @@ void PelvisControlInterface::controlPelvisHeight(float height)
     msg.taskspace_trajectory_points.clear();
     msg.taskspace_trajectory_points.push_back(p);
     msg.use_custom_control_frame = false;
-
 
 
     // publish the message
