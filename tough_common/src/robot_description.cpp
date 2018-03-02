5794d3d00e7dcc2bcdbacbebd0fe9be4984f0c09
Vinayak Jagtap <vinayak.jagtap04@gmail.com>
2018-03-02 10:26:24 -0500

    using robot_name as a parameter instead of deriving it from urdf. This is due to the robot_description parameter present under robot_name namespace. It might be useful "IF" we plan to use atlas and valkyrie at the time.

--- tough_common/src/robot_description.cpp
+++ tough_common/src/robot_description.cpp
@@ -44,7 +44,14 @@ RobotDescription::RobotDescription(ros::NodeHandle nh, std::string urdf_param)
 
     std::string robot_xml;
 
-    if(!nh.getParam("/"+robot_name_+urdf_param, robot_xml) || !model_.initString(robot_xml)){
+    if(!nh.getParam("ihmc_ros/robot_name", robot_name_)){
+        ROS_ERROR("Could not read the robot_name parameter.");
+    }
+    else {
+        urdf_param = "/"+robot_name_+urdf_param;
+    }
+
+    if(!nh.getParam(urdf_param, robot_xml) || !model_.initString(robot_xml)){
         ROS_ERROR("Could not read the robot_description");
         return;
     }
@@ -52,6 +59,9 @@ RobotDescription::RobotDescription(ros::NodeHandle nh, std::string urdf_param)
         robot_name_.assign(model_.getName());
     }
 
+//    robot_name_ = model_.getName();
+    ROS_INFO("Robot Name : %s", robot_name_.c_str());
+    std::string prefix = "/ihmc_ros/" + robot_name_ + "/";
 
     param_left_arm_joint_names_.insert(0, prefix);
     param_right_arm_joint_names_.insert(0, prefix);
--- tough_common/src/robot_state.cpp
+++ tough_common/src/robot_state.cpp
@@ -14,15 +14,15 @@ RobotStateInformer* RobotStateInformer::getRobotStateInformer(ros::NodeHandle nh
 }
 
 RobotStateInformer::RobotStateInformer(ros::NodeHandle nh):nh_(nh){
-//    ROS_INFO("Object Created");
-    nh.getParam(TOUGH_COMMON_NAMES::ROBOT_NAME_PARAM, robotName_);
-    jointStateSub_ = nh_.subscribe("/ihmc_ros/"+robotName_+"/output"+TOUGH_COMMON_NAMES::JOINT_STATES_TOPIC, 1, &RobotStateInformer::jointStateCB, this);
+
+    rd_ = RobotDescription::getRobotDescription(nh_);
+    nh.getParam("ihmc_ros/robot_name", robotName_);
+
+    jointStateSub_ = nh_.subscribe("ihmc_ros/" + robotName_ + "/output/joint_states", 1, &RobotStateInformer::jointStateCB, this);
     ros::Duration(0.2).sleep();
     closeRightGrasp={1.09,1.47,1.84,0.90,1.20,1.51,0.99,1.34,1.68,0.55,0.739,0.92,1.40};
     closeLeftGrasp={0.0,-1.47,-1.84,-0.90,-1.20,-1.51,-0.99,-1.34,-1.68,-0.55,-0.739,-0.92,1.40};
     openGrasp={0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
-    rd_ = RobotDescription::getRobotDescription(nh_);
-
 
 }
 
