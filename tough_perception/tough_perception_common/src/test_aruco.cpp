79fe0582352102caf5ea25a38518c7427c30336b
AmeyaWagh <ameya555@gmail.com>
2019-02-22 23:24:20 -0500

    Added id map example to test_aruco.cpp

--- tough_perception/tough_perception_common/src/test_aruco.cpp
+++ tough_perception/tough_perception_common/src/test_aruco.cpp
@@ -19,13 +19,17 @@ int main(int argc, char** argv)
 
     std::map<std::string,geometry_msgs::PoseStampedPtr> detected_objects;
 
+    std::map<std::string, std::string> object_ids;
+    object_ids["10"] = "Gantry Front";
+    object_ids["15"] = "Gantry Back";
+
     for(int i=0;i<100;i++)
     {
         detector.getDetectedObjects(detected_objects);
 
         for (std::map<std::string, geometry_msgs::PoseStampedPtr>::iterator it = detected_objects.begin(); it != detected_objects.end(); it++ )
         {
-            printPoseMessages(it->first,it->second);        
+            printPoseMessages(object_ids[it->first],it->second);        
         }
     }
     
