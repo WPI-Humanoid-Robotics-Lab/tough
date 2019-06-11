a1846cf0860adbdaa33a0838b0e342d157dd9398
AshayAswale <asaswale@wpi.edu>
2019-06-11 19:34:28 -0400

    simplified the head_control_example

--- tough_examples/src/control_examples/head_control_example.cpp
+++ tough_examples/src/control_examples/head_control_example.cpp
@@ -6,50 +6,29 @@
 
 int main(int argc, char** argv)
 {
-  const float degToRad = M_PI / 180.0f;
-  ros::init(argc, argv, "test_head_navigation", ros::init_options::AnonymousName);
-  ros::NodeHandle nh;
-  HeadControlInterface* headTraj;
-
-  ros::Publisher log_pub = nh.advertise<std_msgs::String>(TOUGH_COMMON_NAMES::LOG_TOPIC, 10);
-  const auto log_msg = [&log_pub](const std::string& str) {
-    std_msgs::String msg;
-    msg.data = ros::this_node::getName() + ": " + str;
-    log_pub.publish(msg);
-    ROS_INFO("%s", msg.data.c_str());
-  };
-
-  // wait a reasonable amount of time for the subscriber to connect
-  ros::Time wait_until = ros::Time::now() + ros::Duration(0.5);
-  while (log_pub.getNumSubscribers() == 0 && ros::Time::now() < wait_until)
-  {
-    ros::spinOnce();
-    ros::WallDuration(0.1).sleep();
-  }
-
-  headTraj = new HeadControlInterface(nh);
 
   if (argc != 4)
   {
-    log_msg("Unexpected arguments, expected roll pitch yaw (in degrees). Exiting.");
-    return 1;
+    std::string filename = std::string(argv[0]);
+    int index = filename.find_last_of('/');
+    std::string input_trace_filename = filename.substr(index + 1);
+    ROS_INFO_STREAM("Usage : rosrun tough_examples " << input_trace_filename
+                                                     << " <roll in degrees> <pitch in degrees> <yaw in degrees>");
+    return -1;
   }
   else
   {
+    const float degToRad = M_PI / 180.0f;
+    ros::init(argc, argv, "test_head_navigation", ros::init_options::AnonymousName);
+    ros::NodeHandle nh;
+    HeadControlInterface headTraj(nh);
     ROS_INFO("Moving the head");
     float roll = std::atof(argv[1]) * degToRad;
     float pitch = std::atof(argv[2]) * degToRad;
     float yaw = std::atof(argv[3]) * degToRad;
 
-    log_msg("Moving head to direction " + std::to_string(roll) + ", " + std::to_string(pitch) + ", " +
-            std::to_string(yaw));
-    headTraj->moveHead(roll, pitch, yaw);
+    headTraj.moveHead(roll, pitch, yaw);
+    ros::Duration(2).sleep();
+    return 0;
   }
-
-  ros::spinOnce();
-  ros::Duration(2).sleep();
-  log_msg("Motion finished");
-
-  delete headTraj;
-  return 0;
 }
